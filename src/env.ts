f1b7aac9ada454a2af924ba257d674289bd42519
har <arim44@naver.com>
2026-05-13 16:20:14 +0900

    feat: create  weatherfit-app

---
+++ src/env.ts
@@ -0,0 +1,7 @@
+
+// .env 
+export const ENV={
+    API_URL:process.env.NEXT_PUBLIC_API_URL || 'test.co.kr',
+    IMAGE_URL:process.env.NEXT_PUBLIC_IMAGE_URL || '',
+    API_KEY:process.env.VITE_OPEN_WEATHER_KEY || '',
+};

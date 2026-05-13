d80d581a53051cdbf758e5b5a2fd208ee2502576
har <arim44@naver.com>
2026-05-13 16:45:59 +0900

    feat: add header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import style from './globals.css'
 
 export default function RootLayout({
   children,
@@ -10,7 +11,10 @@ export default function RootLayout({
       <body>
         <div>
           <header>
-            <Link href='./'> 🌤️ WeatherFit</Link>
+            {/* 링크로 하면 미리가져오는 프리패칭을 함 */}
+            <Link href={'/'}>홈 🌤️ WeatherFit</Link> &nbsp;&nbsp;
+            <Link href={'/outfit'}>코디추천</Link> &nbsp;&nbsp;
+            <Link href={'/todo'}>todo</Link> &nbsp;&nbsp;
           </header>
           <main>{children} </main>
           <footer>&copy; 2026 양파마켓 All right reseved</footer>
